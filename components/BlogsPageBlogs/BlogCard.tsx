a627bd5e61c46ae5b6a7f7b1288f266c5a917432
Yuchen Liu <lyc990312@hotmail.com>
2024-09-14 19:09:40 +1000

    Change the layour for desktop view

--- components/BlogsPageBlogs/BlogCard.tsx
+++ components/BlogsPageBlogs/BlogCard.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { motion, useInView } from "framer-motion"
+import { useRef } from "react";
 import { formatDate } from "../../utilities/format-data"
 
 interface Children {
@@ -23,8 +26,12 @@ interface BlogCardProps {
   }
 }
 export default function BlogCard({ componentData }: BlogCardProps) {
+
+  const showUpRef = useRef(null)
+  const isInView = useInView(showUpRef, { once: true })
+
   return (
-    <div className="max-w-[350px] md:max-w-[350px] p-7 shadow-xl mx-auto min-h-[500px] md:max-h-[550px] hover:bg-slate-100 transition hover:shadow-2xl">
+    <div className="max-w-[350px] my-3 md:max-w-[380px] p-7 shadow-xl mx-auto min-h-[500px] md:max-h-[550px] hover:bg-slate-100 transition hover:shadow-2xl">
       <div className="max-w-[350px] h-[250px] overflow-hidden">
         <img
           className="h-[200px] object-cover"
@@ -36,26 +43,40 @@ export default function BlogCard({ componentData }: BlogCardProps) {
       <div className="font-semibold text-gray-500">
         {formatDate(componentData.postTime)}
       </div>
-      <a className="flex items-start hover:underline" href={`/blogs/${componentData.slug}`}>
-        <div className="font-bold text-2xl my-2">{componentData.title}</div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="w-5 min-w-5 mt-2 ml-1"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 19L19 5M5 5h14v14"
-          />
-        </svg>
-      </a>
-      <div className="multiline-ellipsis text-lg my-2 text-gray-500">
-        {componentData.content[0].children[0].text}
-      </div>
+      <motion.div
+      ref={showUpRef}
+      initial={{ y: 15, opacity: 0.3 }}
+      animate={{
+        y: isInView ? 0 : 15,
+        opacity: isInView ? 1 : 0.3,
+      }}
+      transition={{
+        duration: 0.7,
+        delay: 0.3
+      }}
+      >
+        <a className="flex items-start hover:underline" href={`/blogs/${componentData.slug}`}>
+          <div className="font-bold text-2xl my-2">{componentData.title}</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-5 min-w-5 mt-2 ml-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 19L19 5M5 5h14v14"
+            />
+          </svg>
+        </a>
+        <div className="multiline-ellipsis text-lg my-2 text-gray-500">
+          {componentData.content[0].children[0].text}
+        </div>
+      </motion.div>
+
     </div>
   )
 }
