8328d37bb0d0b390ba692336caa1892b16e6ef4a
Yuchen Liu <lyc990312@hotmail.com>
2024-09-17 15:09:33 +1000

    Modified the getAPI function, it can switch api based on node env

--- utilities/get-api.ts
+++ utilities/get-api.ts
@@ -1,6 +1,7 @@
-// import * as dotenv from "dotenv"
-// dotenv.config()
-
 export function getAPI(): string | undefined {
-  return `http://127.0.0.1:1337/api/`
+  if (process.env.NODE_ENV === 'production') {
+    return process.env.STRAPI_API
+  } else {
+    return process.env.LOCALHOST_API
+  }
 }
