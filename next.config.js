c716594d33db959fd6ef4819bfbc78240f5c19e7
Yuchen Liu <lyc990312@hotmail.com>
2024-09-17 15:15:10 +1000

    Add the credential config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
     SEND_EMAIL_API: process.env.SEND_EMAIL_API,
     ADD_SUBSCRIBE_API: process.env.ADD_SUBSCRIBE_API,
     GOOGLE_MAP_ADDRESS_URL: process.env.GOOGLE_MAP_ADDRESS_URL,
-    WEBSITE_URL: process.env.WEBSITE_URL
+    WEBSITE_URL: process.env.WEBSITE_URL,
+    STRAPI_API:process.env.STRAPI_API,
+    LOCALHOST_API:process.env.LOCALHOST_API
   }
 }
 
