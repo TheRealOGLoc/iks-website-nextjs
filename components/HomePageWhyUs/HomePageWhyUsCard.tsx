db33d8c63fc578cdf97334a11863320c6435fd30
Yuchen Liu <lyc990312@hotmail.com>
2024-09-18 16:46:07 +1000

    Test the potential fix of the framer motion div blink again

--- components/HomePageWhyUs/HomePageWhyUsCard.tsx
+++ components/HomePageWhyUs/HomePageWhyUsCard.tsx
@@ -13,18 +13,18 @@ export default function HomePageWhyUsCard({ image, title, text, index }: HomePag
 
   const showUpRef = useRef(null)
   const isInView = useInView(showUpRef, { once: true })
-  const delayTime = (index + 1) * 0.1
+  const delayTime = (index + 1) * 0.2
 
   return (
     <motion.div
       key={index}
       ref={showUpRef}
-      initial={{ opacity: 0.5}}
+      initial={{ opacity: 0.5, y: 20}}
       animate={{
         opacity: isInView ? 1 : 0.5,
-
+        y: isInView ? 0 : 20
       }}
-      transition={{ duration: 0.5, ease:"easeInOut", delay: delayTime }}
+      transition={{ duration: 0.5, delay: delayTime }}
       className="inter-font bg-gray-600 hover:bg-gray-500 transition mx-2 my-2 p-5 md:p-7 rounded-md text-center w-[calc(50%-1rem)] md:w-[390px] text-white">
       <img className="w-[49px] m-auto" src={image.icon.data.attributes.url} alt={image.icon.data.attributes.alternativeText} title={image.icon.data.attributes.caption} />
       <div className="font-bold my-2">{title}</div>
