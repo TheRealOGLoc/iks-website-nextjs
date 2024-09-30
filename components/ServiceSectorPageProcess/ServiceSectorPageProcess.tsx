93273392c015a0b8bbcc7bbdc7d7f451586983d6
Yuchen Liu <lyc990312@hotmail.com>
2024-09-30 18:02:55 +1000

    Make process to one line

--- components/ServiceSectorPageProcess/ServiceSectorPageProcess.tsx
+++ components/ServiceSectorPageProcess/ServiceSectorPageProcess.tsx
@@ -16,17 +16,17 @@ export default function ServiceSectorPageProcess({ componentData }: ServiceSecto
   return (
     <div className="">
       <div className="text-center poppins-font text-5xl font-bold text-gray-500"><span>{componentData.titleLeftPart}</span> <span className="light-blue">{componentData.titleRightPart}</span></div>
-      <div className="flex flex-wrap flex-col xl:flex-row max-w-[1418px] m-[auto] mt-5">
+      <div className="flex flex-col md:flex-row max-w-[1418px] m-[auto] mt-5">
         {
           componentData.card && componentData.card.map((content: any, index: number) => {
 
-            return <div className="flex flex-col xl:flex-row items-center" key={index}>
+            return <div className="flex flex-col md:flex-row items-center" key={index}>
               {
                 <ProcessCard title={content.title} description={content.description} icon={content.icon} index={index} key={index} />
               }
               {
                 index !== 3 &&
-                <Image src={DotArrow} className="rotate-90 xl:rotate-0 w-[64px] h-[64px] mx-1" alt={""} />
+                <Image src={DotArrow} className="rotate-90 md:rotate-0 w-[64px] h-[64px] mx-1" alt={""} />
               }
             </div>
           })
