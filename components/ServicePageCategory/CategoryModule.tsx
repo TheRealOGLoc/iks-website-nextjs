c4394d6cb275635d1a3d580ef1bc8811ee3710fd
Yuchen Liu <lyc990312@hotmail.com>
2024-09-30 18:48:50 +1000

    Align the text

--- components/ServicePageCategory/CategoryModule.tsx
+++ components/ServicePageCategory/CategoryModule.tsx
@@ -6,7 +6,7 @@ interface CategroyModuleProps {
 
 export default function CategoryModule({title, description}: CategroyModuleProps) {
   return (
-    <div className="max-w-[360px] relative">
+    <div className="max-w-[360px] relative text-center">
       <TextCover/>
       <div className="poppins-font text-2xl font-semibold">{title}</div>
       <div className="inter-font">{description}</div>
