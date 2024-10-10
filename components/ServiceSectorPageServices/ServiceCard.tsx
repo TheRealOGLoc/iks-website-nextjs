3a1824297287f8cf426e0c0d2093f7dbf9946b96
IKS-ADMIN-GIT <infinikey-solutions-github@outlook.com>
2024-10-10 13:12:41 +1000

    Add the error handler

--- components/ServiceSectorPageServices/ServiceCard.tsx
+++ components/ServiceSectorPageServices/ServiceCard.tsx
@@ -10,10 +10,9 @@ interface ServiceCardProps {
 }
 
 export default function ServiceCard({ title, descriptionOne, descriptionTwo, descriptionThree, image }: ServiceCardProps) {
-  
   return (
     <div className="max-w-[360px] border-[2px] border-gray-300 rounded-3xl py-3 m-5 md:m-3">
-      <img className="rounded-full w-[192px] h-[192px] my-10 mx-auto" src={image.data.attributes.url} alt={image.data.attributes.alternativeText} title={image.data.attributes.caption} />
+      <img className="rounded-full w-[192px] h-[192px] my-10 mx-auto" src={image?.data?.attributes?.url} alt={image.data.attributes.alternativeText} title={image.data.attributes.caption} />
       {title && <div className="text-center h-14 font-semibold text-2xl poppins-font my-1">{title}</div>}
       <ul className="pl-3 pr-4 inter-font">
         {descriptionOne && <li className="flex mb-2" ><Image src={Tick} alt="" className="w-[24px] h-[24px] mr-3" /> {descriptionOne}</li>}
